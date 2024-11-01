093f50c34e08fb4aa998dd2cdeb9146628c825ec
Humberto de Moraes Lanzieri Filho <134455113+hlanzieri@users.noreply.github.com>
2024-11-01 10:33:31 -0300

    Create Array.js

---
+++ 3 bimestre/Array.js
@@ -0,0 +1,13 @@
+//var Nome = ["Humberto"];
+//var Endereco = ["Ponte Alta"];
+//var Telefone = [3234211010];
+//console.log(Nome[0]);
+//console.log(Endereco[0]);
+//console.log(Telefone[0]);
+
+var Dados = {
+
+    "Humberto": {endereco: "Avenida Jesus, 176 - Cataguases", telefone: "34211100"}
+}
+
+console.log( "Dados do Humberto: "+ Dados["Humberto"].endereco +"  "+ Dados["Humberto"].telefone);
