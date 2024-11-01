aadefd4aaacc069bea19413d157a65e8afd26eb5
Humberto de Moraes Lanzieri Filho <134455113+hlanzieri@users.noreply.github.com>
2024-11-01 09:55:06 -0300

    Create conversao.js

---
+++ 3 bimestre/conversao.js
@@ -0,0 +1,8 @@
+var n=17;
+var binary_string=n.toString(2);
+var octal_string="0"+n.toString(8);
+var hex_string="0x"+n.toString(16);
+
+console.log(binary_string);
+console.log(octal_string);
+console.log(hex_string);
