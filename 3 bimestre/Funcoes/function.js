00967e6fa5d4041f77964ba4884909e3db400d38
Humberto de Moraes Lanzieri Filho <134455113+hlanzieri@users.noreply.github.com>
2024-11-08 10:33:43 -0300

    Add files via upload

---
+++ 3 bimestre/Funcoes/function.js
@@ -0,0 +1,12 @@
+//function nomefunc(arg1, arg2, arg3){
+    //instruções}
+
+
+    function hypotenuse ( x , y ){
+    return Math.sqrt(x*x+y*y);
+    }
+    function factorial (n){
+    if (n <= 1) return 1;
+    return n*factorial (n−1) ;
+    }
+    
