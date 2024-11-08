b7c2ac56d29d8fb648c708dbf8a8926ca9ac18d9
Humberto de Moraes Lanzieri Filho <134455113+hlanzieri@users.noreply.github.com>
2024-11-08 09:17:18 -0300

    Update Array.js

--- 3 bimestre/Array.js
+++ 3 bimestre/Array.js
@@ -5,9 +5,13 @@
 //console.log(Endereco[0]);
 //console.log(Telefone[0]);
 
-var Dados = {
+var Dados= {
 
-    "Humberto": {endereco: "Avenida Jesus, 176 - Cataguases", telefone: "34211100"}
+    "Humberto": {endereco: "Avenida Jesus, 176 - Cataguases", telefone: "34211100"},
+    "Nicolas": {endereco: "Rua das Flores, 29 - Cataguases", telefone:"34210001"},
+    "Diego": {endereco: "Ponte Alta, 24 - Cataguases", telefone:"342111111"}
 }
 
 console.log( "Dados do Humberto: "+ Dados["Humberto"].endereco +"  "+ Dados["Humberto"].telefone);
+console.log( "Dados do Nicolas: "+ Dados["Nicolas"].endereco +"  "+ Dados["Nicolas"].telefone);
+console.log( "Dados do Diego: "+ Dados["Diego"].endereco +"  "+ Dados["Diego"].telefone);
